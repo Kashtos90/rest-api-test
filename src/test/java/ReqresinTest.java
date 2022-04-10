b941f1ace6992aa7826e7536b79dbd2c009d1c21
Sergei Kashtuev <29061990a@gmail.com>
2022-04-10 17:19:11 +0700

    added tests

---
+++ src/test/java/ReqresinTest.java
@@ -0,0 +1,50 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import io.restassured.response.Response;
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.is;
+
+public class ReqresinTest {
+
+    @Test
+    @DisplayName("Проверка емейла")
+    void checkDataEmail() {
+        get("https://reqres.in/api/users/2")
+            .then()
+                .body("data.email", is("janet.weaver@reqres.in"));
+    }
+
+    @Test
+    @DisplayName("Проверка имени")
+    void checkFirstName() {
+        get("https://reqres.in/api/users/2")
+                .then()
+                .body("data.first_name", is("Janet"));
+    }
+
+    @Test
+    @DisplayName("Проверка фамилии")
+    void checkLastName() {
+        get("https://reqres.in/api/users/2")
+                .then()
+                .body("data.last_name", is("Weaver"));
+    }
+
+    @Test
+    @DisplayName("Проверка URl'a")
+    void checkUrl() {
+        get("https://reqres.in/api/users/2")
+                .then()
+                .body("support.url", is("https://reqres.in/#support-heading"));
+    }
+
+    @Test
+    @DisplayName("Проверка текста")
+    void checkText() {
+        get("https://reqres.in/api/users/2")
+                .then()
+                .body("support.text", is("To keep ReqRes free, contributions towards server costs are appreciated!"));
+    }
+}
