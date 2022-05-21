634c2cbf766619046ed8de7904c9afb984b93888
Sergei Kashtuev <29061990a@gmail.com>
2022-05-21 20:43:36 +0700

    added fixes

--- src/test/java/ReqresinTest.java
+++ src/test/java/ReqresinTest.java
@@ -2,9 +2,12 @@ import models.Morpheus;
 import models.UserData;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static specs.Specs.request;
 import static specs.Specs.responseSpec;
@@ -16,10 +19,13 @@ public class ReqresinTest {
     @Test
     @DisplayName("Обновление пользователя Morpheus")
     public void UpdateMorpheus () {
+
+        step("Обновляем данные о Morpheus");
         Morpheus morpheus = new Morpheus();
         morpheus.setName("morpheus");
         morpheus.setJob("zion resident");
 
+        step("Отправляем запрос на обновление данных");
         given()
                 .spec(requestSpec)
                 .body(morpheus)
@@ -29,11 +35,17 @@ public class ReqresinTest {
                 .spec(responsemorpheusSpec)
                 .body(matchesJsonSchemaInClasspath("userdata.json"))
                 .extract().as(Morpheus.class);
+
+        step("Проверяем полученные в ответе данные");
+        assertThat(morpheus.getName()).isEqualTo("morpheus");
+        assertThat(morpheus.getJob()).isEqualTo("zion resident");
     }
 
     @Test
     @DisplayName("Проверка наличия списка пользователей")
     public void usersListTest() {
+
+        step("Отправляем запрос о списке пользователей");
         UserData data = given()
                 .spec(request)
                 .when()
@@ -42,6 +54,8 @@ public class ReqresinTest {
                 .spec(responseSpec)
                 .log().body()
                 .extract().as(UserData.class);
+
+        step("Проверяем совпадение данных пользователей в ответе");
         assertEquals(7, data.getUser()[0].getId());
     }
 }
