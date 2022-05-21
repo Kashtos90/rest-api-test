2c41a0cbab25fad7f37b8174e91cf44bee58aa5f
Sergei Kashtuev <29061990a@gmail.com>
2022-05-21 18:36:32 +0700

    added specs

---
+++ src/test/java/specs/Specs.java
@@ -0,0 +1,22 @@
+package specs;
+
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+import static io.restassured.RestAssured.with;
+import static listeners.CustomAllureListener.withCustomTemplates;
+
+public class Specs {
+
+    public static RequestSpecification request = with()
+            .filter(withCustomTemplates())
+            .baseUri("https://reqres.in")
+            .log().body()
+            .contentType(ContentType.JSON);
+
+    public static ResponseSpecification responseSpec = new ResponseSpecBuilder()
+            .expectStatusCode(200)
+            .build();
+}
